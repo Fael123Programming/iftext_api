8cdfc52efd36eac217ccaa4cbb9717571751a89c
Fael123Programming <rafaelfonseca1020@gmail.com>
2024-07-09 12:24:13 -0300

    Feat.: summarization endpoint

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from transformers import pipeline
+from transformers import pipeline, BartForConditionalGeneration, BartTokenizer
 from fastapi.middleware.cors import CORSMiddleware
-
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust this to your frontend URL
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,11 +17,12 @@ sentiment_analyzer = pipeline(
     'sentiment-analysis',
     'nlptown/bert-base-multilingual-uncased-sentiment'
 )
-
+model_name = "facebook/bart-large-cnn"
+tokenizer = BartTokenizer.from_pretrained(model_name)
+model = BartForConditionalGeneration.from_pretrained(model_name)
 
 class TextRequest(BaseModel):
     text: str
-
 
 @app.post('/sentiment-analysis/')
 async def sentiment_analysis(request: TextRequest):
@@ -32,8 +32,17 @@ async def sentiment_analysis(request: TextRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.post('/summarization/')
+async def summarization(request: TextRequest):
+    try:
+        inputs = tokenizer(request.text, max_length=1024, return_tensors="pt", truncation=True)
+        summary_ids = model.generate(inputs['input_ids'], num_beams=4, min_length=30, max_length=150, early_stopping=True)
+        summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        return {'summarization': summary_text}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get('/')
 async def root():
-    return {'message': 'Sentiment Analysis API'}
+    return {'message': 'NLP (Natural Language Processing) API'}
 
